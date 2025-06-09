49f79ffaac2b9c98e9d7c19a54ebc22cea9dbf57
PauloCN01 <paulo.ferreira3@estudante.ifms.edu.br>
2025-06-09 11:06:58 -0300

    Container

---
+++ src/Container.tsx
@@ -0,0 +1,10 @@
+//States
+//Prosp -> informações que você pode passar para um componente
+//Hooks
+
+function Container() {
+    return (
+        <h1>Header</h1>
+    )
+}
+export default Container
