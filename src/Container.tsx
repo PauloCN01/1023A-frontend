c9eedd330f35e5d25e44de164424be656e3c33e6
PauloCN01 <paulo.ferreira3@estudante.ifms.edu.br>
2025-06-10 11:17:26 -0300

    atividade texto feita

--- src/Container.tsx
+++ src/Container.tsx
@@ -6,16 +6,16 @@ interface ContainersProps{
     nome:string
 }
 function Container(props:ContainersProps) {
-    const [contador,setContador] = useState(0)
-    function mudar(){
-        setContador(20)
-        console.log(contador)
+    const [texto, setTexto] = useState("Mude esse texto")
+    function trataInput(event:React.ChangeEvent<HTMLInputElement>){
+        setTexto(event.currentTarget.value)
+        console.log(event.currentTarget.value)
     }
     return(
     <>
       <h1>{props.nome}</h1>
-      Valor Contador:{contador}
-      <button onClick={mudar}>Mudar</button>
+      <div>Texto:{texto}</div>
+      <input type="text" placeholder="Mostrar Texto" onChange={trataInput}/>
     </>
     )
 }
